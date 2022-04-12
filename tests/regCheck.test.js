18181721c38cde81696c1b1753d6824ec54c5d59
Anthony <almogotlane@gmail.com>
2022-04-12 16:09:42 +0200

    update test

--- tests/regCheck.test.js
+++ tests/regCheck.test.js
@@ -1,18 +1,17 @@
 describe("regCheck", function() {
-    it("The registration number COD 456 MP ends with MP, thats correct.", function() {
-        assert.deepEqual(regCheck("COD 456 MP", "MP"), true)
+    it("The registration number 'COD 456 MP' ends with 'MP', thats correct.", function() {
+        assert.deepEqual(regCheck("COD 456 MP", "MP"), true);
+    })j
+
+    it("The registration number 'EXP 491 L' ends with 'L', thats correct.", function() {
+        assert.deepEqual(regCheck("EXP 491 L", "L"), true);
     })
 
-    it("The registration number EXP 491 L ends with L, thats correct.", function() {
-        assert.deepEqual(regCheck("EXP 491 L", "L"), true)
+    it("The registration number 'YH 45 HN GP' does not end with 'EC', that is incorrect", function() {
+        assert.deepEqual(regCheck("YH 45 HN GP", "EC"), false);
     })
 
-    it("The registration number YH 45 HN GP ends with GP not EC, that is incorrect", function() {
-        assert.deepEqual(regCheck("YH 45 HN GP", "EC"), false)
+    it("The registration number 'FHN 679 EC' does not end with 'MP', that is incorrect", function() {
+        assert.deepEqual(regCheck("FHN 679 EC", "MP"), false);
     })
-
-    it("The registration number FHN 679 EC ends with EC not MP, that is incorrect", function() {
-        assert.deepEqual(regCheck("FHN 679 EC", "MP"), false)
-    })
-
 })
